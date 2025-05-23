16782bbb9ef308f1503c652cd66a4fe225c22b38
Yves Thommes <wickeddoc@wickeddoc.com>
2025-05-23 15:24:59 +0200

    add basic, and slow, support for mouse movement not great, not terrible

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"flag"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const (
-	screenWidth  = 1024
-	screenHeight = 768
-	playerSize   = 8
-	playerStep   = playerSize / 2
+	screenWidth          = 1024
+	screenHeight         = 768
+	playerSize   float32 = 8.0
+	playerStep   float32 = playerSize / 2.0
 )
 
 // Player variables
@@ -19,12 +20,56 @@ var (
 // Draw player in 2D view
 func drawPlayer2D() {
 	// Draw player point
-	rl.DrawRectangle(int32(posX), int32(posY), playerSize, playerSize, rl.Red)
+	rl.DrawRectangle(int32(posX), int32(posY), int32(playerSize), int32(playerSize), rl.Red)
 
 }
 
+// Handle player movement with mouse
+func movePlayerWithMouse() {
+	// Get mouse position
+	mousePos := rl.GetMousePosition()
+
+	// Calculate direction vector from player to mouse
+	dirX := mousePos.X - posX
+	dirY := mousePos.Y - posY
+
+	// Calculate distance
+	distance := rl.Vector2Distance(rl.NewVector2(posX, posY), mousePos)
+
+	// Only move if mouse is not too close to player (to prevent jittering)
+	if distance > playerSize {
+		// Normalize direction vector and apply movement speed
+		speed := playerStep
+		if rl.IsMouseButtonDown(rl.MouseLeftButton) {
+			speed = playerStep * 2.0 // Move faster when left mouse button is pressed
+		}
+
+		// Calculate new position with normalized direction
+		if distance > 0 {
+			// Move with normalized direction vector
+			normalizedDirX := dirX / distance
+			normalizedDirY := dirY / distance
+			posX += normalizedDirX * speed
+			posY += normalizedDirY * speed
+		}
+
+		// Ensure player stays within screen bounds
+		if posX < 0 {
+			posX = 0
+		} else if posX > screenWidth-playerSize {
+			posX = screenWidth - playerSize
+		}
+
+		if posY < 0 {
+			posY = 0
+		} else if posY > screenHeight-playerSize {
+			posY = screenHeight - playerSize
+		}
+	}
+}
+
 // Handle player movement
-func movePlayer() {
+func movePlayerWithKeyboard() {
 	// Move up
 	if rl.IsKeyDown(rl.KeyW) && posY > 0 {
 		posY = posY - playerStep
@@ -47,14 +92,27 @@ func movePlayer() {
 }
 
 func main() {
+
+	useMouse := flag.Bool("mouse", false, "Use mouse for player movement")
+	flag.Parse()
+
 	// Initialize Raylib
 	rl.InitWindow(screenWidth, screenHeight, "Raycaster in Go using Raylib")
 	rl.SetTargetFPS(60)
 
+	if *useMouse {
+		rl.HideCursor()
+	}
+
 	// Main game loop
 	for !rl.WindowShouldClose() {
 		// Update
-		movePlayer()
+		// Update
+		if *useMouse {
+			movePlayerWithMouse()
+		} else {
+			movePlayerWithKeyboard()
+		}
 
 		// Draw
 		rl.BeginDrawing()
@@ -63,10 +121,18 @@ func main() {
 		// Draw player
 		drawPlayer2D()
 
-		// Display controls
-		rl.DrawText("W,A,S,D: Move", 10, screenHeight-30, 20, rl.DarkGray)
+		// Display controls based on mode
+		if *useMouse {
+			rl.DrawText("Mouse: Move, Left Click: Speed up", 10, screenHeight-30, 20, rl.DarkGray)
+		} else {
+			rl.DrawText("W,A,S,D: Move", 10, screenHeight-30, 20, rl.DarkGray)
+		}
 
 		rl.EndDrawing()
+	}
+
+	if *useMouse {
+		rl.ShowCursor()
 	}
 
 	// Clean up resources
